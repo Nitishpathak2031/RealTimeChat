7f0f120f46b3a7fc44033fdb4aa88d2bca73e014
Nitish <nitishpathak203@gmail.com>
2025-10-21 19:15:10 +0530

    port changed

--- backend/src/server.js
+++ backend/src/server.js
@@ -24,4 +24,4 @@ if(process.env.NODE_ENV == "production"){
     })
 }
 
-app.listen(3000, () => console.log("server is running on port 3000"))
+app.listen(port, () => console.log(`server is running on port ${port}`))
