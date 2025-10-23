feedf1d430651093b5a8f5ca6d476a3a6b922a83
Nitish <nitishpathak203@gmail.com>
2025-10-23 20:58:12 +0530

    login and logout completed

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -49,3 +49,37 @@ export const signup = async(req,res)=>{
         res.status(500).json({message:"internal server error"})
     }
 };
+
+export const login = async(req,res)=>{
+const {email , password} = req.body
+try{
+const user = await User.findOne({email});
+
+if(!user) return res.status(400).json({message:"invaild credentials"});
+
+const ispasswordCorrect = await bcrypt.compare(password,user.password)
+
+if(!ispasswordCorrect) return res.status(400).json({message:"invaild credentials"});
+
+generateToken(user.id,res)
+
+
+res.status(200).json({
+    _id:user._id,
+    fullname:user.fullname,
+    email: user.email,
+    profilepic:user.profilepic
+});
+}
+catch(error){
+console.error("Error in login controller:",error);
+res.status(500).json({message:"internal server error"});
+}
+
+}
+
+
+export const logout = async(req,res)=>{
+    res.cookie("jwt","",{maxAge:0});
+    res.status(200).json({message:"logout successfully"})
+}
