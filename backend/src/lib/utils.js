dc0237cfd6767dc67733f652cdcf6ac26ae767d5
Nitish <nitishpathak203@gmail.com>
2026-01-15 21:25:11 +0530

    jwt fix

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -7,7 +7,8 @@ export const generateToken = (UserId,res) =>{
     res.cookie("jwt",token,{
         maxAge:7*24 * 60* 60*1000,
         httpOnly:true,
-        sameSite:"Strict",
+        sameSite:"none",
+        secure:"true",
         secure:process.env.NODE_ENV === "development" ? false :true,
     });
 
